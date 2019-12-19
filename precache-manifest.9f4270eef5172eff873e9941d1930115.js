eda1171bbc7e48db97a1c898ebd9155cfb830021
Ziv <ziv.israeli01@gmail.com>
2019-12-19 13:08:11 -0800

    Updates

---
+++ precache-manifest.9f4270eef5172eff873e9941d1930115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28fc73889973a4bba1c2ca17e40fc934",
+    "url": "/mia-test/index.html"
+  },
+  {
+    "revision": "8672f0ac4321c372c94a",
+    "url": "/mia-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fbee59f25f7ebe34a1a9",
+    "url": "/mia-test/static/js/2.819836d6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mia-test/static/js/2.819836d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8672f0ac4321c372c94a",
+    "url": "/mia-test/static/js/main.04af907e.chunk.js"
+  },
+  {
+    "revision": "a876cbda63840867f961",
+    "url": "/mia-test/static/js/runtime-main.0835dd82.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mia-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
